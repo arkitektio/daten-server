ab59a2462daffd639c9907483a7fff2279a0eed5
Johannes Roos <jhnnsrs@gmail.com>
2024-08-19 22:57:04 +0200

    added age

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM postgres:16
+FROM apache/age:latest
 
 COPY ./create-multiple-databases.sh /docker-entrypoint-initdb.d/create-multiple-databases.sh 
 RUN chmod +x  /docker-entrypoint-initdb.d/create-multiple-databases.sh
--- create-multiple-databases.sh
+++ create-multiple-databases.sh
@@ -11,6 +11,12 @@ function create_user_and_database() {
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
 EOSQL
+
+	echo "  Loading AGE extension into database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$database" <<-EOSQL
+	    CREATE EXTENSION IF NOT EXISTS age;
+	    LOAD 'age';
+EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
